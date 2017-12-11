30d79360aa91ab5bb2156f7dc19cab7069930d28
Jerry <Jerry@DESKTOP-VAJ1D8N>
2017-12-10 23:58:58 -0500

    initial

--- FinalExam/src/main/java/pkgCore/Retirement.java
+++ FinalExam/src/main/java/pkgCore/Retirement.java
@@ -8,23 +8,92 @@ public class Retirement {
 	private double dAnnualReturnRetired;
 	private double dRequiredIncome;
 	private double dMonthlySSI;
-	
-	//TODO: Build the contructor, getters and setters for the attributes above.
-	
-	public double AmountToSave()
-	{
-		//TODO: Determine the amount to save each month based on TotalAmountSaved, YearsToWork
-		//		and Annual return while working
-		
-		
-		return 0;
+
+	public Retirement(int iYearsToWork, double dAnnualReturnWorking, int iYearsRetired, double dAnnualReturnRetired,
+			double dRequiredIncome, double dMonthlySSI) {
+		super();
+		this.iYearsToWork = iYearsToWork;
+		this.dAnnualReturnWorking = dAnnualReturnWorking;
+		this.iYearsRetired = iYearsRetired;
+		this.dAnnualReturnRetired = dAnnualReturnRetired;
+		this.dRequiredIncome = dRequiredIncome;
+		this.dMonthlySSI = dMonthlySSI;
 	}
-	
-	public double TotalAmountSaved()
-	{
-		//	TODO: Determine amount to be saved based on Monthly SSI, Required Income, Annual return during retirement
-		//		and number of years retired.
-		//
-		return 0;
+
+	public Retirement() {
+
+	}
+
+	public double AmountToSave(double TotalAmountSAved) {
+		double dARK = dAnnualReturnWorking / 12;
+		double dMW = iYearsToWork * 12;
+		double ReturnVal = 0;
+		if (dARK == 0) {
+			ReturnVal = TotalAmountSaved() / dARK;
+		} else {
+			ReturnVal = TotalAmountSaved() * dARK / (Math.pow(1 + dARK, dARK) - 1);
+		}
+		return ReturnVal;
+	}
+
+	public double TotalAmountSaved() {
+		double dARR = dAnnualReturnRetired / 12;
+		double ReturnVal = 0;
+		int dMR = iYearsRetired * 12;
+		if (dARR == 0) {
+			ReturnVal = iYearsRetired * (dRequiredIncome - dMonthlySSI);
+		} else {
+			ReturnVal = (dRequiredIncome - dMonthlySSI) * ((Math.pow(1 + dARR, dMR) - 1) / dARR)
+					/ Math.pow(1 + dARR, dMR);
+		}
+		return ReturnVal;
+	}
+
+	public int getiYearsToWork() {
+		return iYearsToWork;
+	}
+
+	public void setiYearsToWork(int iYearsToWork) {
+		this.iYearsToWork = iYearsToWork;
+	}
+
+	public double getdAnnualReturnWorking() {
+		return dAnnualReturnWorking;
+	}
+
+	public void setdAnnualReturnWorking(double dAnnualReturnWorking) {
+		this.dAnnualReturnWorking = dAnnualReturnWorking;
+	}
+
+	public int getiYearsRetired() {
+		return iYearsRetired;
+	}
+
+	public void setiYearsRetired(int iYearsRetired) {
+		this.iYearsRetired = iYearsRetired;
+	}
+
+	public double getdAnnualReturnRetired() {
+		return dAnnualReturnRetired;
+	}
+
+	public void setdAnnualReturnRetired(double dAnnualReturnRetired) {
+		this.dAnnualReturnRetired = dAnnualReturnRetired;
+	}
+
+	public double getdRequiredIncome() {
+		return dRequiredIncome;
+	}
+
+	public void setdRequiredIncome(double dRequiredIncome) {
+		this.dRequiredIncome = dRequiredIncome;
+	}
+
+	public double getdMonthlySSI() {
+		return dMonthlySSI;
+	}
+
+	public void setdMonthlySSI(double dMonthlySSI) {
+		this.dMonthlySSI = dMonthlySSI;
 	}
 }
---
+++ FinalExam/src/test/java/pkgEmpty/TESTRetirement.java
@@ -0,0 +1,17 @@
+package pkgEmpty;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pkgCore.Retirement;
+
+public class TESTRetirement {
+
+	@Test
+	public void testRetirement() {
+		Retirement r = new Retirement(40,0.07,20,0.02,10000,2642);
+		assertEquals(r.TotalAmountSaved(),1454485.55,0.001);
+		assertEquals(r.AmountToSave(r.TotalAmountSaved()),554.13,0.001);
+	}
+}
